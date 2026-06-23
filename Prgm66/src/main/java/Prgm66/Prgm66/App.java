3021792c23cd0270c8d61bac0780c8f2343cb5c8
ISE <ISE@DESKTOP-ETN0263>
2026-06-23 12:39:13 +0530

    First Commit

---
+++ Prgm66/src/main/java/Prgm66/Prgm66/App.java
@@ -0,0 +1,13 @@
+package Prgm66.Prgm66;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
